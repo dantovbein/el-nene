cc75cf89fd8968121533c298c7a0f508e55d6a6c
Dan <dtovbein@gmail.com>
2015-02-18 08:25:33 -0300

    Sitio primera version

--- service/sendEmail.php
+++ service/sendEmail.php
@@ -6,12 +6,7 @@ if(isset($_POST['userName'])){ $userName = $_POST['userName']; };
 if(isset($_POST['userLastName'])){ $userLastName = $_POST['userLastName']; };
 if(isset($_POST['userComment'])){ $userComment = $_POST['userComment']; };
 
-/*if(isset($_GET['userEmail'])) { $userEmail = $_GET['userEmail']; };
-if(isset($_GET['userName'])){ $userName = $_GET['userName']; };
-if(isset($_GET['userLastName'])){ $userLastName = $_GET['userLastName']; };
-if(isset($_GET['userComment'])){ $userComment = $_GET['userComment']; };*/
-//$to = 'dtovbein@gmail.com';
-$to = 'gordillo.nico@gmail.com';
+$to = 'info@estrada.com.ar';
 $subject = 'Mensaje de ' . $userName . ' ' . $userLastName . ' a través de blockelnene.com.ar';
 $message = $userComment;
 $headers = 'From: ' . $userEmail . "\r\n" .
